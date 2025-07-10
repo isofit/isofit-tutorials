1fc32d4b6d53821e470271bdfc26854e5450d6f3
Evan Greenberg <ev.ben.green@gmail.com>
2025-07-09 17:18:50 -0700

    adding 6c lut and editing config for glint model

---
+++ 20151026_SantaMonica/lut/LUT_AOT550-0.7003_H2OSTR-1.4000.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+/Users/bgreenbe/Github/6sMultiComponentFN/Sixs/sixsV2.1 < /Users/bgreenbe/Projects/IsofitDev/TestCases/Glint/lut_full/LUT_AOT550-0.7003_H2OSTR-1.4000.inp > /Users/bgreenbe/Projects/IsofitDev/TestCases/Glint/lut_full/AOT550-0.7003_H2OSTR-1.4000
+cd $cwd
