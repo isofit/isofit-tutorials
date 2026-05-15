4bc15d8b0d3f4a70fc987ae90a41bb94af3125ca
Evan Greenberg <ev.ben.green@gmail.com>
2026-05-15 06:10:59 -0600

    Removing local files

--- 20250308_AV3Cal_wltest/lut_full/LUT_AOT550-0.0500_H2OSTR-2.2000.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-"/Users/bgreenbe/.isofit/sixs/sixsV2.1.2.CO2" < "/Users/bgreenbe/Projects/IsofitDev/2026/WavelengthDebug/lut_full/LUT_AOT550-0.0500_H2OSTR-2.2000.inp" > "/Users/bgreenbe/Projects/IsofitDev/2026/WavelengthDebug/lut_full/AOT550-0.0500_H2OSTR-2.2000"
-cd $cwd
